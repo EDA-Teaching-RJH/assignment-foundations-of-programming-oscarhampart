47ed09a2331c9c9f1bcc29135997ceafa33fad9d
oscarhampart <oh230@kent.ac.uk>
2026-02-16 01:19:20 +0000

    fuelConsumption

--- old_system.py
+++ old_system.py
@@ -87,11 +87,11 @@ def run_system_monolith():
 
         
         fuel = 100
-        consumption = 0
+        consumption = 10
+        
         while fuel > 0:
-            
             print("Idling...")
-            break 
+            fuel = fuel - consumption
             
         print("End of cycle.")
 
