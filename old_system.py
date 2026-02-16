c72468296e30404e03e8b55b302b30625d8122e0
oscarhampart <oh230@kent.ac.uk>
2026-02-16 00:26:21 +0000

    rangelenn

--- old_system.py
+++ old_system.py
@@ -26,11 +26,11 @@ def run_system_monolith():
         
         opt = input("Select option: ")
         
-        if opt == "1":  
+        if opt == "1":
             print("Current Crew List:")
             
-            for i in range(10):
-                print(n[i] + " - " + r[i]) 
+            for i in range(len(n)):
+                print(n[i] + " - " + r[i])
                 
         elif opt == "2":
             new_name = input("Name: ")
