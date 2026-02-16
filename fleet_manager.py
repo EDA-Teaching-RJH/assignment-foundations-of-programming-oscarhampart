506ce47d5b9f1b5efd6225542fe1e04f944cdfb4
oscarhampart <oh230@kent.ac.uk>
2026-02-16 03:59:51 +0000

    init_database()

--- fleet_manager.py
+++ fleet_manager.py
@@ -2,8 +2,17 @@ def main():
     print("Welcome to Fleet Manager.")
     display_menu()
 
+def init_database():
+    names = ["Picard", "Riker", "Data", "Worf", "Kirk"]
+    ranks = ["Captain", "Commander", "Lt. Commander", "Lieutenant", "Captain"]
+    divs = ["Command", "Command", "Operations", "Security", "Command"]
+    ids = ["SP-937-215", "SC-231-427", "NFN NMI Data", "Son of Mogh", "SC 937-0176 CEC"]
+    
+    return names, ranks, divs, ids
+
 
 def display_menu():
+    names, ranks, divs, ids = init_database()
     fullName = input("What is your full name?").strip().title()
     print("Welcome", fullName)
     
